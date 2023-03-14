1e71df129edc79bbaaced321ed83f3b5d30cb960
Christophe Tiraoui <tuxounet@gmail.com>
2023-03-14 23:19:03 +0100

    init serving

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,6 +1,6 @@
 import { router } from "./context";
 import helloRouter from "./features/hello";
-import inventoryRouter from "./features/inventory";
+import inventoryRouter from "./features/inventory/router";
 import templatesRouter from "./features/templates";
 export const appRouter = router({
   hello: helloRouter,
